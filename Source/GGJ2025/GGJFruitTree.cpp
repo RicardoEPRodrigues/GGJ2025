8653e8dcc55a07709fa0a79895576faf0d27b672
Manuel Guimarães <manuel_guimaraes@hotmail.com>
2025-01-24 12:50:01 +0000

    feat: Adding GGJFruittree classes

---
+++ Source/GGJ2025/GGJFruitTree.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GGJFruitTree.h"
+
+#include "Algo/RandomShuffle.h"
+
+
+// Sets default values
+AGGJFruitTree::AGGJFruitTree()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AGGJFruitTree::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGGJFruitTree::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+FVector3f AGGJFruitTree::GetFruitSpawnPosition()
+{
+	// Returns the position within the circle
+	FVector CenterPosition;
+	CenterPosition = this->GetActorLocation();
+
+	CenterPosition = CenterPosition + FVector(0, 0, SpawnHeight);
+
+	float randomAngle = FMath::RandRange(0,360); 
+	
+	float X = CenterPosition.X + (SpawnCircleRadius * FMath::Cos(randomAngle));
+	float Y = CenterPosition.Y + (SpawnCircleRadius * FMath::Sin(randomAngle));
+
+	FVector3f SpawnPosition = FVector3f(X, Y, SpawnHeight);
+
+	return SpawnPosition;	
+}
+
---
+++ Source/GGJ2025/GGJFruitTree.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GGJFruitTree.generated.h"
+
+UCLASS()
+class GGJ2025_API AGGJFruitTree : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AGGJFruitTree();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Basic Attribute Set
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tree")
+	float SpawnCircleRadius;
+
+	// Basic Attribute Set
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tree")
+	float SpawnHeight;
+
+	UFUNCTION(BlueprintCallable, Category = "Tree")
+	FVector3f GetFruitSpawnPosition();
+};
