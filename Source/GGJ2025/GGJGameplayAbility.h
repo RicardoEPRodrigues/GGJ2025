f1a47a31e965405d614c8ed743ed21ab31fa4927
Manuel Guimarães <manuel_guimaraes@hotmail.com>
2025-01-26 01:15:08 +0000

    Added GGJGameplay Ability Code, improved Dash and Jump. Changed fruit falling variables.

---
+++ Source/GGJ2025/GGJGameplayAbility.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GGJGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ2025_API UGGJGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GGJ")
+	FString Name = "";
+};
